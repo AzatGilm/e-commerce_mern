5a98d3011bdff4ae75d7ed357db1a670da77ff54
azat <ya.azat77@yandex.ru>
2024-03-27 13:14:42 +0300

    with api

---
+++ api/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/user");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+
+dotenv.config();
+const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("DB Connection successfull");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+
+app.listen(process.env.PORT || 5000, (req, res) => {
+  console.log("app listening port 5000");
+});
