d4de4ee23e62f6db6d87a7329fc3ec9135b56567
azat <ya.azat77@yandex.ru>
2024-03-27 10:53:24 +0300

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,21 @@ import Product from "./pages/Product";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const user = true;
   return (
-    <div className="App">
-      <Home/>;
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
